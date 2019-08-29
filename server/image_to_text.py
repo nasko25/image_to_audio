48e4c9a0f38c70ebb22dddef3b787f3a22ba140a
nasko25 <atanas.pashov@yahoo.com>
2019-08-29 15:33:15 +0200

    fixed an encoding bug and removed new lines

--- server/image_to_text.py
+++ server/image_to_text.py
@@ -12,17 +12,17 @@ import cv2
 import sys 
 import os 
 
-sys.stdout = open('reached_python.txt', 'a')
+# sys.stdout = open('reached_python.txt', 'a')
 print "reached python code; image file name: " , str(sys.argv)
 # os.system("touch reached_python")
 
 # The script will be called whenever the user uploads an image to be conveted.
 # Firstly, it will spawn a new process/thread, which will create a random value and create a folder with a name that is the random value. If such a folder already exists, the script will save an image proccessed with (page_dewarp and) pytesseract there as a txt file with a random name. 
 
-image = cv2.imread(sys.argv[1])
-print(pytesseract.image_to_string(Image.fromarray(image)))
+print(pytesseract.image_to_string(Image.open(sys.argv[1])).encode("utf-8").replace("\n", " "))
 
 # It could also format the txt file (remove \n).
+# Maybe also remove the things from the very top and very bottom (like titles/authors and page numbers on a book)
 # Then it will call another script, passing the name of the folder, and the file name (as a path), and will save an audio file with the random name. 
 # Then it will somehow send the audio file (either as a POST response or by email).
 # Finally, the script will delete the file, wait for a second, and delete the folder and then kill the thread.
