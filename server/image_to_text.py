be1afe512aa3df3f77ec88ec3f0e8e5f45690ec7
nasko25 <atanas.pashov@yahoo.com>
2019-08-31 19:22:42 +0200

    added an option to specify if the user wants to use page_dewarp

--- server/image_to_text.py
+++ server/image_to_text.py
@@ -22,11 +22,16 @@ print "reached python code; image file name: " , str(sys.argv)
 name = sys.argv[1].partition("/")[2].partition(".")[0]
 # print(name)
 # exit()
-os.system("python page_dewarp/page_dewarp.py " + sys.argv[1])
-os.system("mv " + name + "_thresh.png uploads/")
-print(pytesseract.image_to_string(Image.open("uploads/" + name + "_thresh.png")).encode("utf-8").replace("\n", " "))
+if len(sys.argv) >=3 and sys.argv[2] == "pd":
+    os.system("python page_dewarp/page_dewarp.py " + sys.argv[1])
+    os.system("mv " + name + "_thresh.png uploads/")
+    print(pytesseract.image_to_string(Image.open("uploads/" + name + "_thresh.png")).encode('utf-8').replace("\n", " "))
+else:
+    print(pytesseract.image_to_string(Image.open(sys.argv[1])).encode('utf-8').replace("\n", " "))
+
 
 # It could also format the txt file (remove \n).
+# If there is a - followed by a space (because the new line was converted to a space) remove the dash and the space (as it most likely is a single word on two lines)
 # Maybe also remove the things from the very top and very bottom (like titles/authors and page numbers on a book) (they are usually separated by several new lines)
 # Also tell the user to crop the image, so that there is only text from one page at a time; and take as good of a photo as possible.
 # Then it will call another script, passing the name of the folder, and the file name (as a path), and will save an audio file with the random name. 
