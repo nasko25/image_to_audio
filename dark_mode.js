cc33cfedf3ba314c38bc25b2b875a90202eee76b
nasko25 <apashov0@gmail.com>
2020-03-08 17:31:57 +0100

    added a cookie for the dark mode and implemented dark mode on the description page

--- dark_mode.js
+++ dark_mode.js
@@ -1,3 +1,29 @@
+if (getCookie("dm")===""){
+  document.cookie = "dm=1";
+} else if (getCookie("dm") === "1" && !document.getElementById('dark_mode_link')) {
+  setDarkMode();
+} else if (getCookie("dm") === "0") {
+  if (document.getElementById("dark_mode"))
+    document.getElementById("dark_mode").checked = false;
+  setLightMode();
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
 function on_dark_mode() {
   dark_mode_toogle = document.getElementById("dark_mode");
   // get <link rel="stylesheet" href="main.css">
@@ -11,15 +37,25 @@ function on_dark_mode() {
     $('link[href="dark_mode.css"]').attr('href','main.css');
   } */
 
-  if(dark_mode_toogle.checked == true && !document.getElementById('dark_mode_link')) {
-    var dark_mode_link = document.createElement('link');
-    dark_mode_link.rel = 'stylesheet';
-    dark_mode_link.href = 'dark_mode.css';
-    dark_mode_link.id = 'dark_mode_link';
-    document.head.appendChild(dark_mode_link);
+  if((dark_mode_toogle.checked == true && !document.getElementById('dark_mode_link'))) {
+    setDarkMode();
+    document.cookie = "dm=1";
   }
   else {
-    var dark_mode_id = document.getElementById("dark_mode_link");
-    dark_mode_id.parentNode.removeChild(dark_mode_id);
+    setLightMode();
+    document.cookie = "dm=0";
   }
+}
+
+function setDarkMode() {
+  var dark_mode_link = document.createElement('link');
+  dark_mode_link.rel = 'stylesheet';
+  dark_mode_link.href = 'dark_mode.css';
+  dark_mode_link.id = 'dark_mode_link';
+  document.head.appendChild(dark_mode_link);
+}
+
+function setLightMode() {
+  var dark_mode_id = document.getElementById("dark_mode_link");
+  dark_mode_id.parentNode.removeChild(dark_mode_id);
 }
